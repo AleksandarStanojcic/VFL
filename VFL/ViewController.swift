84458fdb1e1ea9c7ef31aaf69c13d3abd85f4492
aleksandarstanojcic <aleksandar.stanojcic1983@gmail.com>
2017-04-13 15:38:25 +0200

    Initial commit

---
+++ VFL/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  VFL
+//
+//  Created by Aleksandar Stanojcic on 2/28/17.
+//  Copyright © 2017 Aleksandar Stanojcic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var upperLeft: UIView!
+    var middle: UIView!
+    var bottomRight: UIView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createViews()
+        constrainViews()
+    }
+
+    func createViews() {
+        
+        upperLeft = UIView()
+        upperLeft.translatesAutoresizingMaskIntoConstraints = false
+        upperLeft.backgroundColor = UIColor.red
+        view.addSubview(upperLeft)
+        
+        middle = UIView()
+        middle.translatesAutoresizingMaskIntoConstraints = false
+        middle.backgroundColor = UIColor.blue
+        view.addSubview(middle)
+        
+        bottomRight = UIView()
+        bottomRight.translatesAutoresizingMaskIntoConstraints = false
+        bottomRight.backgroundColor = UIColor.green
+        view.addSubview(bottomRight)
+        
+    }
+    
+    func constrainViews() {
+        
+        let horizontalGap = 8
+        let verticalGap = 20
+        let cubeSize = 50
+        
+        var constraints: [NSLayoutConstraint] = []
+        
+        let metrics = ["horizontalGap": horizontalGap, "verticalGap": verticalGap, "tall": cubeSize, "wide": cubeSize]
+        let views: [String : UIView] = ["upperLeft": upperLeft, "middle": middle, "bottomRight": bottomRight]
+        
+        constraints.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "H:[upperLeft(wide)]-(horizontalGap)-[middle(wide)]-(horizontalGap)-[bottomRight(wide)]", options: [], metrics: metrics, views: views))
+        constraints.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "V:[upperLeft(tall)]-(verticalGap)-[middle(tall)]-(verticalGap)-[bottomRight(tall)]", options: [], metrics: metrics, views:views))
+        constraints.append(middle.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(middle.centerYAnchor.constraint(equalTo: view.centerYAnchor))
+        NSLayoutConstraint.activate(constraints)
+    }
+}
