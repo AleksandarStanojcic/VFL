502faf82354e700bc5af87f41d773d890545f2ed
aleksandarstanojcic <aleksandar.stanojcic1983@gmail.com>
2017-04-19 12:05:42 +0200

    Start Master

--- VFL/ViewController.swift
+++ VFL/ViewController.swift
@@ -28,19 +28,16 @@ class ViewController: UIViewController {
         upperLeft.translatesAutoresizingMaskIntoConstraints = false
         upperLeft.backgroundColor = UIColor.red
         view.addSubview(upperLeft)
-        print("First commit master")
         
         middle = UIView()
         middle.translatesAutoresizingMaskIntoConstraints = false
         middle.backgroundColor = UIColor.green
         view.addSubview(middle)
-        print("Third commit test")
 
         bottomRight = UIView()
         bottomRight.translatesAutoresizingMaskIntoConstraints = false
         bottomRight.backgroundColor = UIColor.yellow
         view.addSubview(bottomRight)
-        print("Third commit master")
     }
     
     func constrainViews() {
